0666f22bdb38a50d69c144ddf08a3eed46b91fbf
Rameez Remsudeen <remzr7@gmail.com>
2016-11-29 01:41:29 -0500

    Added Metal support

--- Lepton/Lepton/Lepton/LPMetalPipeline.swift
+++ Lepton/Lepton/Lepton/LPMetalPipeline.swift
@@ -7,7 +7,52 @@
 //
 
 import Foundation
+import Metal
 
+class MetalPipeline {
+    var device:MTLDevice
+    var library:MTLLibrary?
+    
+    let commandQueue:MTLCommandQueue
+//    let pipelineState:MTLComputePipelineState
+    
+    init(device:MTLDevice) {
+        self.device = device
+        self.commandQueue = device.makeCommandQueue()
+        self.library = device.newDefaultLibrary()
+    }
+    
+    func textureForImage(image:LPImage) -> MTLTexture? {
+        let height = image.height
+        let width = image.width
+        
+        let textureDesc = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: .rgba8Uint, width: width, height: height, mipmapped: false)
+        
+        let region = MTLRegionMake2D(0, 0, height, width)
+        let texture = device.makeTexture(descriptor: textureDesc)
+        
+        guard let rawData = image.pixels.baseAddress else {
+            return nil
+        }
+        
+        let bytesPerRow = MemoryLayout<Pixel>.size*width
+        
+        texture.replace(region: region, mipmapLevel: 0, withBytes: rawData, bytesPerRow: bytesPerRow);
+        
+        return texture
+    }
+    
+    func textureForMask(mask:LPMask) -> MTLTexture {
+        let textureDescriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: .r32Float, width: mask.width, height: mask.height, mipmapped: false)
+        
+        let maskTexture = device.makeTexture(descriptor: textureDescriptor)
+        let region = MTLRegionMake2D(0, 0, mask.height, mask.width)
+        maskTexture.replace(region: region, mipmapLevel: 0, withBytes: mask.mask, bytesPerRow: MemoryLayout<Float>.size * mask.width)
+        
+        return maskTexture
+        
+    }
+}
 public func createComputePipeline(functionName:String) {
 //    let functionName = MTLDevice.newFunctionWithName(name)
 }
