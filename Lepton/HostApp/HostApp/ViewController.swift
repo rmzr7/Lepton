5dd9e4ef3925aa9a8647301b246a75fd72eb1064
William Tong <wxt@andrew.cmu.edu>
2016-12-10 00:24:45 -0500

    changed to kmeansGPU in view controller

--- Lepton/HostApp/HostApp/ViewController.swift
+++ Lepton/HostApp/HostApp/ViewController.swift
@@ -23,7 +23,8 @@ class ViewController: UIViewController {
         //let gaussian3 = lepton.GaussianFilterGenerator(1.0)
         //let x = lepton.blurImage(image!, mask: gaussian3)
         //let x = lepton.acceleratedBlurImageCPU(image!, mask:gaussian3)
-        let newImage = kmeans.kmeansSegment(image!);
+        let newImage = kmeans.KMeansGPU(image!);
+        //let newImage = kmeans.kmeansSegment(image!);
         
         let imageView = UIImageView(image: newImage)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
